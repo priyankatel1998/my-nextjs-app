4b47a49b5027e7578dd02b7b4a16400ded48430c
priyankatel1998 <priyankatel1998@gmail.com>
2025-11-15 16:30:23 +0530

    my third commit about new message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      Hello from next js , Hello I am priyanka
+      This is priyanka . I am commiting my third commit 
     </div>
   );
 }
