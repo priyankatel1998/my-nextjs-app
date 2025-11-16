4e0f1da2688bc8c4639b658e3f8f7b253bc3c88c
priyankatel1998 <priyankatel1998@gmail.com>
2025-11-16 06:46:19 +0530

    Adding pag layout for UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="bg-gray-100 text-gray-900">
+        {/* <Navbar /> */}
+        <main className="max-w-4xl mx-auto p-6">{children}</main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      This is priyanka . I am commiting my third commit 
+      This is priyanka . I am commiting my third commit  I write firty commit in gitbash 
     </div>
   );
 }
